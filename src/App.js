233736eab84d2cb6479fcda973478bedaf168387
Ramkumar96 <sivaram0203@gmail.com>
2019-08-27 15:47:54 +0530

    new app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React,{Component} from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class App extends Component {
+  render() {
+    return (
+      
+        <div className="container">
+          <h2>MERN-Stack Todo App</h2>
+        </div>
+      
+    );
+  }
+}
+
+export default App;
