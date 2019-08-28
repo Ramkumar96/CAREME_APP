016abecd9517402b34f01d28b1b8c4e20e171160
Ramkumar96 <sivaram0203@gmail.com>
2019-08-28 12:47:43 +0530

    Home page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
 import React,{Component} from "react";
+//import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+
 
 class App extends Component {
   render() {
     return (
-      
-        <div className="container">
-          <h2>MERN-Stack Todo App</h2>
-        </div>
-      
+         <div>
+         <nav class="navbar navbar-expand-lg px-4 bg-light">
+            <a class="navbar-brand" href="#">
+              Care Me
+            </a>
+
+            {/* <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#myNavbar">
+              <span class="toggler-icon"> 
+                <i class="fas fa-bars"></i>
+              </span> 
+            </button> */}
+
+            <a class="navbar-brand" href="#">
+              How it works
+            </a>
+            <div>
+            <button type="button" class="btn btn-outline-primary">Login</button>
+            </div>
+          </nav>
+
+              <div class="container-fluid">
+                <div class="row max-height justify-content-center align-items-center">
+                  <div class="col-10 mx-auto banner text-center">
+                      <h1 class="text-capitalize">
+                        welcome to <strong class="banner-title">Care Me</strong>
+                      </h1>
+                        <span>
+                        <a href="#store" class="btn banner-link text-uppercase my-2">I want a Nurse</a>
+                        </span>
+                        <span>
+                        <a href="#store" class="btn banner-link text-uppercase my-2">I want a Patient</a>
+                        </span>
+                  </div>
+                </div> 
+              </div>
+        </div>           
     );
   }
 }
