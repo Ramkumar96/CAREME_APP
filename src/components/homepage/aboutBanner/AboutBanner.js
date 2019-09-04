ebadd31932cffe6c8202a7c9d34e844f2f5ada53
Ramkumar96 <sivaram0203@gmail.com>
2019-09-04 22:47:52 +0530

    about banner

--- src/components/homepage/aboutBanner/AboutBanner.js
+++ src/components/homepage/aboutBanner/AboutBanner.js
@@ -2,45 +2,34 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 //import classes from "./IntroBanner.css";
-import ReactBootstrap, {Jumbotron, Button, Col,Row,Container,Grid, Panel, FormGroup} from 'react-bootstrap'
+//import ReactBootstrap, {Jumbotron, Button, Col,Row,Container,Grid, Panel, FormGroup} from 'react-bootstrap'
 
 
 class AboutBanner extends Component{
     render(){
         return(
             <div>
-                <Container>
-  {/* Stack the columns on mobile by making one full-width and the other half-width */}
-  <Row>
-    <Col xs={12} md={8}>
-      xs=12 md=8
-    </Col>
-    <Col xs={6} md={4}>
-      xs=6 md=4
-    </Col>
-  </Row>
-
-  {/* Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop */}
-  <Row>
-    <Col xs={6} md={4}>
-      xs=6 md=4
-    </Col>
-    <Col xs={6} md={4}>
-      xs=6 md=4
-    </Col>
-    <Col xs={6} md={4}>
-      xs=6 md=4
-    </Col>
-  </Row>
-
-  {/* Columns are always 50% wide, on mobile and desktop */}
-  <Row>
-    <Col xs={6}>xs=6</Col>
-    <Col xs={6}>xs=6</Col>
-  </Row>
-</Container>
-            </div>
-        );
+                <div className="container">
+                    <div className="row">
+                        <div className="col-10 mx-auto col-md-6 my-5">
+                            <h1>How <strong>Careme </strong> </h1>
+                            <h1 className="align-center">help you...</h1>
+                            <p className="text-muted">
+                             we are learning to code never will help you to find a nurse
+                             we are learning to code never will help you to find a nurse
+                             we are learning to code never will help you to find a nurse
+                             </p>
+                             <a href="#" class="btn btn-black text-uppercase">explore</a>
+                        </div>   
+                        <div className="col-10">
+                            <div>
+                                <h1>Image Image</h1>
+                            </div>
+                        </div> 
+                    </div>
+                </div>
+            </div> 
+       );
     }
 }
 
