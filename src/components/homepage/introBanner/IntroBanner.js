bcac837800d94a9e33f8b0b2e15ea66e507a1681
Ramkumar96 <sivaram0203@gmail.com>
2019-09-18 00:07:33 +0530

    format register of clients form

--- src/components/homepage/introBanner/IntroBanner.js
+++ src/components/homepage/introBanner/IntroBanner.js
@@ -111,48 +111,62 @@ class IntroBanner extends Component{
                                         <span>
                                             <input type="button" class="btn btn-secondary btn-lg" value="I WANT A NURSE" onClick={() => this.openModal1()} />
                                             <Modal visible={this.state.visible1} width="50%" height="85%" effect="fadeInUp" onClickAway={() => this.closeModal1()}>
-                                            <h1>Register Here</h1>
-                                                <Form>
-                                                <Form.Row>
-                                                    <Form.Group as={Col} controlId="FirstName">
-                                                    <Form.Label>First Name</Form.Label>
-                                                    <Form.Control type="text" placeholder="Enter First Name" /> 
-                                                    </Form.Group>
+                                           
+                                                <Form className="form-margin">
+                                                    <h4>Create a CareMe account</h4>
+                                                    <br/>
+                                                    <div class="form-group row">
+                                                        <label for="firstname" class="col-sm-2 col-form-label">Frist Name</label>
+                                                            <div class="col-sm-10">
+                                                                <input type="text" class="form-control" id="firstname"/>
+                                                            </div>
+                                                    </div>
 
-                                                    <Form.Group as={Col} controlId="LastName">
-                                                    <Form.Label>Last Name</Form.Label>
-                                                    <Form.Control type="text" placeholder="Enter Last Name" /> 
-                                                    </Form.Group>
-                                                </Form.Row>
+                                                    <div class="form-group row">
+                                                        <label for="lastname" class="col-sm-2 col-form-label">Last Name</label>
+                                                            <div class="col-sm-10">
+                                                                <input type="text" class="form-control" id="lastname"/>
+                                                            </div>
+                                                    </div>
 
-                                                <Form.Group controlId="emailAd">
-                                                    <Form.Label>E-mail Address</Form.Label>
-                                                    <Form.Control type="email" placeholder="janedoe@example.com" />
-                                                </Form.Group>
+                                                    <div class="form-group row">
+                                                        <label for="emailaddress" class="col-sm-2 col-form-label">E-mail</label>
+                                                            <div class="col-sm-10">
+                                                                <input type="email" class="form-control" id="emailaddress"/>
+                                                            </div>
+                                                    </div>
 
-                                                <Form.Row>
-                                                    <Form.Group as={Col} controlId="Password">
-                                                    <Form.Label>Password</Form.Label>
-                                                    <Form.Control type="password" placeholder="Enter Your Password Here"/> 
-                                                    </Form.Group>
+                                                    <div class="form-group row">
+                                                        <label for="password" class="col-sm-2 col-form-label">Password</label>
+                                                            <div class="col-sm-10">
+                                                                <input type="password" class="form-control" id="password"/>
+                                                            </div>
+                                                    </div>
 
-                                                    <Form.Group as={Col} controlId="ConfirmPW">
-                                                    <Form.Label>Confirm Password</Form.Label>
-                                                    <Form.Control type="password" placeholder="Re-Enter Your Password Here" /> 
-                                                    </Form.Group>
-                                                </Form.Row>
+                                                    <div class="form-group row">
+                                                        <label for="re-enter password" class="col-sm-2 col-form-label">Re-enter Password</label>
+                                                            <div class="col-sm-10">
+                                                                <input type="password" class="form-control" id="repassword"/>
+                                                            </div>
+                                                    </div>
 
-                                                <Form.Group controlId="homeAd">
-                                                    <Form.Label>Address</Form.Label>
-                                                    <Form.Control type="textarea" placeholder="Address"/>
-                                                </Form.Group>
+                                                    <div class="form-group row">
+                                                        <label for="address" class="col-sm-2 col-form-label">Address</label>
+                                                            <div class="col-sm-10">
+                                                                <input type="text" class="form-control" id="address"/>
+                                                            </div>
+                                                    </div>
 
-                                                <Form.Group controlId="telNo">
-                                                    <Form.Label>Telephone Number</Form.Label>
-                                                    <Form.Control type="text" placeholder="07########" />
-                                                </Form.Group>
+                                                    <div class="form-group row">
+                                                        <label for="telephone" class="col-sm-2 col-form-label">Telephone</label>
+                                                            <div class="col-sm-10">
+                                                                <input type="text" class="form-control" id="telephone"/>
+                                                            </div>
+                                                    </div>
 
-                                                <Button variant="primary" type="submit">Submit</Button>
+                                                    <Button variant="primary" type="submit">Submit</Button>
+
+                                                    
                                                 </Form>
                                             </Modal>
                                         </span>
