9a048c272d04d5b33ec897773fa2015d6902e78d
Ramkumar96 <sivaram0203@gmail.com>
2019-10-29 10:38:44 +0530

    profile info

--- src/components/profiles/ProfileInfo.js
+++ src/components/profiles/ProfileInfo.js
@@ -21,84 +21,59 @@ export default class ProfileInfo extends Component {
                         <div className="tab-pane" id="profile">
                         {/* The timeline */}
                         <div className="timeline timeline-inverse">
-                            {/* timeline time label */}
-                            <div className="time-label">
-                            <span className="bg-danger">
-                                10 Feb. 2014
-                            </span>
-                            </div>
-                            {/* /.timeline-label */}
-                            {/* timeline item */}
+                        
                             <div>
-                            <i className="fas fa-envelope bg-primary" />
                             <div className="timeline-item">
-                                <span className="time"><i className="far fa-clock" /> 12:05</span>
-                                <h3 className="timeline-header"><a href="#">Support Team</a> sent you an email</h3>
-                                <div className="timeline-body">
-                                Etsy doostang zoodles disqus groupon greplin oooj voxy zoodles,
-                                weebly ning heekya handango imeem plugg dopplr jibjab, movity
-                                jajah plickers sifteo edmodo ifttt zimbra. Babblely odeo kaboodle
-                                quora plaxo ideeli hulu weebly balihoo...
-                                </div>
-                                <div className="timeline-footer">
-                                <a href="#" className="btn btn-primary btn-sm">Read more</a>
-                                <a href="#" className="btn btn-danger btn-sm">Delete</a>
-                                </div>
-                            </div>
-                            </div>
-                            {/* END timeline item */}
-                            {/* timeline item */}
-                            <div>
-                            <i className="fas fa-user bg-info" />
-                            <div className="timeline-item">
-                                <span className="time"><i className="far fa-clock" /> 5 mins ago</span>
                                 <h3 className="timeline-header border-0"><a href="#">Sarah Young</a> accepted your friend request
                                 </h3>
                             </div>
                             </div>
-                            {/* END timeline item */}
-                            {/* timeline item */}
+
                             <div>
-                            <i className="fas fa-comments bg-warning" />
                             <div className="timeline-item">
-                                <span className="time"><i className="far fa-clock" /> 27 mins ago</span>
-                                <h3 className="timeline-header"><a href="#">Jay White</a> commented on your post</h3>
-                                <div className="timeline-body">
-                                Take me to your leader!
-                                Switzerland is small and neutral!
-                                We are more like Germany, ambitious and misunderstood!
-                                </div>
-                                <div className="timeline-footer">
-                                <a href="#" className="btn btn-warning btn-flat btn-sm">View comment</a>
-                                </div>
+                                <h3 className="timeline-header border-0"><a href="#">Sarah Young</a> accepted your friend request
+                                </h3>
                             </div>
                             </div>
-                            {/* END timeline item */}
-                            {/* timeline time label */}
-                            <div className="time-label">
-                            <span className="bg-success">
-                                3 Jan. 2014
-                            </span>
-                            </div>
-                            {/* /.timeline-label */}
-                            {/* timeline item */}
+
                             <div>
-                            <i className="fas fa-camera bg-purple" />
                             <div className="timeline-item">
-                                <span className="time"><i className="far fa-clock" /> 2 days ago</span>
-                                <h3 className="timeline-header"><a href="#">Mina Lee</a> uploaded new photos</h3>
-                                <div className="timeline-body">
-                                <img src="http://placehold.it/150x100" alt="..." />
-                                <img src="http://placehold.it/150x100" alt="..." />
-                                <img src="http://placehold.it/150x100" alt="..." />
-                                <img src="http://placehold.it/150x100" alt="..." />
-                                </div>
+                                <h3 className="timeline-header border-0"><a href="#">Sarah Young</a> accepted your friend request
+                                </h3>
                             </div>
                             </div>
-                            {/* END timeline item */}
+
                             <div>
-                            <i className="far fa-clock bg-gray" />
+                            <div className="timeline-item">
+                                <h3 className="timeline-header border-0"><a href="#">Sarah Young</a> accepted your friend request
+                                </h3>
                             </div>
+                            </div>
+
+                            <div>
+                            <div className="timeline-item">
+                                <h3 className="timeline-header border-0"><a href="#">Sarah Young</a> accepted your friend request
+                                </h3>
+                            </div>
+                            </div>
+
+                            <div>
+                            <div className="timeline-item">
+                                <h3 className="timeline-header border-0"><a href="#">Sarah Young</a> accepted your friend request
+                                </h3>
+                            </div>
+                            </div>
+
+                            <div>
+                            <div className="timeline-item">
+                                <h3 className="timeline-header border-0"><a href="#">Sarah Young</a> accepted your friend request
+                                </h3>
+                            </div>
+                            </div>
+
+                            
+
+                                                     
                         </div>
                         </div>
 
