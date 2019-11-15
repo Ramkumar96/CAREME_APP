29ddc90f6cfa361c485cccf550de9eb6422cf4ce
Ramkumar96 <sivaram0203@gmail.com>
2019-11-16 00:54:17 +0530

    Home page

--- src/components/homepage/aboutBanner/AboutBanner.js
+++ src/components/homepage/aboutBanner/AboutBanner.js
@@ -1,33 +1,45 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import  "./AboutBanner.css";
+import "./AboutBanner.css";
 
-class AboutBanner extends Component{
-    render(){
-        return(
+class AboutBanner extends Component {
+    render() {
+        return (
             <div>
-                <div className="container">
-                    <div className="row">
-                        <div className="col-10 mx-auto col-md-6 my-5">
-                            <h1>How <strong>Careme </strong> </h1>
-                            <h1 className="align-center">help you...</h1>
-                            <p className="text-muted">
-                             we are learning to code never will help you to find a nurse
-                             we are learning to code never will help you to find a nurse
-                             we are learning to code never will help you to find a nurse
-                             </p>
-                             <a href="#" class="btn btn-black text-uppercase">explore</a>
-                        </div>   
-                       
-                        <div className="col-10 mx-auto col-md-6 my-5 align-self-center">
-                            <div className="about-img__container">
-                                <img src= "/images/aboutus.jpg" className="img-fluid" alt="about us "/>
+                <div class="container-fluid">
+                    <div className="row back">
+                        {/* AboutBanner Left Col */}
+                        <div className="col-6 ">
+                            <div className="row">
+                                <img src="/images/about.png" width="225" height="75" alt="aboutus logo " />
                             </div>
-                        </div> 
+                            <div className="row caremeimage">
+                                <img src="/images/caremew.jpg" width="225" height="75" alt="Careme logo " />
+                            </div>
+                            <div className="row abouttext">
+                                <p className="text-muted">
+                                    Care me is a web based system that aims to improve the
+                                    lives of Sri Lankans through home health nursing.Care me connects
+                                    clients who need medical
+                                    assistance and the nurses who would like to work extra.
+                                </p>
+                            </div>
+                            <div className="row abouttext">
+                                <a href="#" class="btn btn-primary btn-lg text-uppercase">explore</a>
+                            </div>
+                        </div>
+
+                        {/* About Banner Right Col */}
+                        <div className="col-6 aboutimg">
+                            <div className="about-img__container">
+                                <img src="/images/aboutus.jpg" className="img-fluid" alt="about us " />
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div> 
-       );
+            </div>
+
+        );
     }
 }
 
