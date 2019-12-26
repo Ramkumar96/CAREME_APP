6aa47cd5154e102164a9653e804b64036dfa0fd4
SumayyaZiyad <sumayyaziyad034@gmail.com>
2019-12-26 13:24:40 +0530

    Minor changes to deac

--- src/components/profiles/nurse/NurseMainPage.js
+++ src/components/profiles/nurse/NurseMainPage.js
@@ -2,10 +2,10 @@ import React, { Component } from "react";
 import ProfileNavbar from '../ProfileNavbar';
 import { BrowserRouter as Router, Link, Redirect } from "react-router-dom";
 import NurseEdit from "../edit/nurseedit";
-import axios from "axios";
 import Modal from 'react-awesome-modal';
 import Calendar from "./Calender";
 import { Button } from 'react-bootstrap';
+import axios from './../../../../backend/node_modules/axios';
 
 class NurseMainPage extends Component {
 
