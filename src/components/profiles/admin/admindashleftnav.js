dec2fa5b2056447e12fc287ff6543259a732068e
emsuds <2017is019@stu.ucsc.cmb.ac.lk>
2020-02-16 10:57:10 +0530

    Final changes

--- src/components/profiles/admin/admindashleftnav.js
+++ src/components/profiles/admin/admindashleftnav.js
@@ -16,7 +16,7 @@ class Admindashleftnav extends Component {
     {/* Sidebar user panel (optional) */}
     <div className="user-panel mt-3 pb-3 mb-3 d-flex">
       <div className="image">
-        <img src="dist/img/user2-160x160.jpg" className="img-circle elevation-2" alt="User Image" />
+        <img src="/images/Admin.png" className="img-circle elevation-2" alt="User Image" />
       </div>
       <div className="info">
         <a href="#" className="d-block">Admin</a>
